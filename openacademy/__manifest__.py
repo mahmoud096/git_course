7e892ad5ca665f5fa96fda8d67889d95352a61e4
mahmoud096 <epuyasystem@gmail.com>
2022-11-19 11:36:27 +0200

    openaccademy

---
+++ openacademy/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': "open academy",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "mahmoud fayez mossa",
+    'category': 'Category',
+    'description': """
+    open academy moduel
+    """,
+    # data files always loaded at installation
+    'data': [
+        #'views/mymodule_view.xml',
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        'demo/demo_data.xml',
+    ],
+}
